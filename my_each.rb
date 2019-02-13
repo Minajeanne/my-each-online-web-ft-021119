582fc23b94955431b4e6806e5ca020f6c6a4cd2c
Alicia Santiago <albrown07@mac.com>
2019-02-13 17:16:55 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,11 @@
 def my_each(num_arr)
   counter = 0 
   while counter < num_arr.length
-    puts num_arr[counter]
+    yield(num_arr[counter])
+    #puts num_arr[counter]
     counter += 1 
+  
   end
 end
 
-def yielding_with_arguments(num)
-  puts "the program is executing the code inside the method"
-  yield(num)
-  puts "now we are back in the method"
-end
 
