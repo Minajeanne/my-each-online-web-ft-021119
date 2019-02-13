79dc5263718c5fbe593909bf56c7475130b60379
Alicia Santiago <albrown07@mac.com>
2019-02-13 17:10:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,9 @@
-def my_each 
-
-
-
+def my_each(num_arr)
+  counter = 0 
+  while counter < num_arr.length
+    puts num_arr[counter]
+    counter += 1 
+  end
 end
 
 def yielding_with_arguments(num)
